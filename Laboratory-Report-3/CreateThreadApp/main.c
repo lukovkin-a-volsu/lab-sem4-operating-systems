151bb9f4646cd678a44b6f213ea560fd403a192f
lukovkin-a-volsu <prib-222_676138@volsu.ru>
2024-03-02 23:14:27 +0300

    work version

---
+++ Laboratory-Report-3/CreateThreadApp/main.c
@@ -0,0 +1,69 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <windows.h>
+#include <stdio.h>
+#include <tchar.h>
+
+DWORD WINAPI ThreadA(void* arg);
+DWORD WINAPI ThreadB(void* arg);
+
+int main(int argc, TCHAR* argv[])
+{
+	DWORD idThreadA;
+	HANDLE hThreadA =
+	CreateThread(
+		NULL,
+		0,
+		ThreadA,
+		NULL,
+		0,
+		&idThreadA
+		);
+
+	if (!SetThreadPriority(hThreadA, THREAD_PRIORITY_IDLE)) {
+		printf("Failed to set hThreadA priority");
+	}
+
+	DWORD idThreadB;
+	HANDLE hThreadB =
+		CreateThread(
+			NULL,
+			0,
+			ThreadB,
+			NULL,
+			0,
+			&idThreadB
+		);
+
+	if (!SetThreadPriority(hThreadB, THREAD_PRIORITY_TIME_CRITICAL)) {
+		printf("Failed to set hThreadB priority");
+	}
+
+	// WaitForSingleObject(hThreadA, INFINITE);
+	//WaitForSingleObject(hThreadA, 1000); // optional
+	WaitForSingleObject(hThreadB, INFINITE);
+
+
+	TerminateThread(hThreadA, 0);
+	TerminateThread(hThreadB, 0);
+
+	return 0;
+}
+
+DWORD WINAPI ThreadA(void* arg) {
+	while (1) {
+		printf("A");
+		Sleep(100);
+	}
+}
+
+DWORD WINAPI ThreadB(void* arg) {
+	long counter = 0;
+	while (1) {
+		printf("B");
+		Sleep(100);
+		if (++counter == 100) {
+			ExitThread(0);
+		}
+	}
+}
